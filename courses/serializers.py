1d9c53929e1f014776df2c27aea43b336fce5c19
Semyon Shtelman <shtelmaan@gmail.com>
2026-05-20 08:28:12 +0600

    Added a field to the serializer to display the number of lessons

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
+
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ('id', 'title', 'description', 'preview', 'lesson_count')
 
 
 class LessonSerializer(serializers.ModelSerializer):
