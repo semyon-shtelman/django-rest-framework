0a06ab02be0a95d9c8a0ebcb9a34fc33fdd9468f
Semyon Shtelman <shtelmaan@gmail.com>
2026-05-24 19:27:37 +0600

    add management commands

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from education.models import Course, Lesson
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title', 'owner'
+    )
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title', 'owner'
+    )
