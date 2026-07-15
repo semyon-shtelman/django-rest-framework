93f7ca8ca4210415e6cc5684eda011225144f485
Semyon Shtelman <shtelmaan@gmail.com>
2026-07-16 00:20:06 +0600

    add password

--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "email", "phone", "city", "avatar")
+        fields = ("id", "email", "phone", "city", "avatar", "password", "password2")
