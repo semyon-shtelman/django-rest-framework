6a89bf449bfb1a3fcf9e2cb06d1395945af6efd2
Semyon Shtelman <shtelmaan@gmail.com>
2026-07-16 00:38:11 +0600

    fix: add password

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,25 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
         fields = ("id", "email", "phone", "city", "avatar", "password", "password2")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "Пароли не совпадают."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        user = User.objects.create_user(**validated_data)
+        return user
